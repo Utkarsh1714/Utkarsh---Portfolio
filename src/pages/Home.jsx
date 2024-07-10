608c110786fb2cfbc511c661c87bc50319ce375a
Utkarsh1714 <utkarshpalav17@gmail.com>
2024-07-10 20:09:50 +0530

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -48,7 +48,7 @@ const Home = () => {
   return (
     <section className=" w-full h-screen relative">
       <div className=' absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-            Popup
+            
         </div>
       <Canvas
         className={` w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
